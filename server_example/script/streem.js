290f4ab182b8c1f46c02f17ddeafb3af74b8f6c4
DPU-820 <masle4ko@gmail.com>
2018-05-27 12:43:19 +0300

    video record

--- server_example/script/streem.js
+++ server_example/script/streem.js
@@ -73,14 +73,13 @@ function addMediaStreamToDiv(divId, stream, streamName, isLocal)
 }
 
 
-
 function createLocalVideo(stream, streamName) {
     var labelBlock = addMediaStreamToDiv("localVideos", stream, streamName, true);
-   // startRecording(stream);
+    startRecord(stream);
     var closeButton = createLabelledButton("close");
     closeButton.onclick = function() {
         easyrtc.closeLocalStream(streamName);
-       //endRecording();
+        endRecord();
         labelBlock.parentNode.parentNode.removeChild(labelBlock.parentNode);
     }
     labelBlock.appendChild(closeButton);
@@ -102,12 +101,6 @@ function addSrcButton(buttonLabel, videoId) {
                 }, buttonLabel);
     };
 }
-
-
-// function hangup() {
-//     easyrtc.hangupAll();
-//     disable('hangupButton');
-// }
 
 
 function clearConnectList() {
@@ -200,13 +193,13 @@ easyrtc.setAcceptChecker(function(easyrtcid, callback) {
     }
     callback(true, easyrtc.getLocalMediaIds());
 });
-// easyrtc.setAcceptChecker(function(easyrtcid, responsefn) {
-//     function myCallFailureCB(errorCode, errorText) {
-//     console.log("call failed ", easyrtcid, errorcode, errorText);
-//     }
-//     responsefn(true, myOutgoingMediaStreams, myCallFailureCB);
-//     document.getElementById("connectbutton_" + easyrtcid).style.visibility = "hidden";
-//     });
+easyrtc.setAcceptChecker(function(easyrtcid, responsefn) {
+    function myCallFailureCB(errorCode, errorText) {
+    console.log("call failed ", easyrtcid, errorcode, errorText);
+    }
+    responsefn(true, myOutgoingMediaStreams, myCallFailureCB);
+    document.getElementById("connectbutton_" + easyrtcid).style.visibility = "hidden";
+    });
 
 function create( name, attributes ) {
     var el = document.createElement( name );
@@ -230,7 +223,7 @@ function create( name, attributes ) {
     return el;
     }
 
-//RECORDING PART
+//OLD RECORDING PART
 ////////////////////////////////
 // var selfRecorder = null;
 // function startRecording(stream) {
@@ -246,3 +239,83 @@ function create( name, attributes ) {
 //        selfRecorder.stop();
 //     }
 // }
+
+//NEW RECORDING PART
+////////////////////////////////
+// fetching DOM references
+var recorder;
+// this function submits recorded blob to nodejs server
+function postFiles() {
+    var blob = recorder.getBlob();
+    // getting unique identifier for the file name
+    var fileName = generateRandomString() + '.webm';
+    
+    var file = new File([blob], fileName, {
+        type: 'video/webm'
+    });
+    xhr("/room/:roomId", file, function(responseText) {
+        var fileURL = JSON.parse(responseText).fileURL;
+
+        console.info('fileURL', fileURL);
+    });
+    if(mediaStream) mediaStream.stop();
+}
+
+// XHR2/FormData
+function xhr(url, data, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(request.responseText);
+        }
+    };
+    request.open('POST', url);
+
+    var formData = new FormData();
+    formData.append('file', data);
+    request.send(formData);
+}
+
+// generating random string
+function generateRandomString() {
+    if (window.crypto) {
+        var a = window.crypto.getRandomValues(new Uint32Array(3)),
+            token = '';
+        for (var i = 0, l = a.length; i < l; i++) token += a[i].toString(36);
+        return token;
+    } else {
+        return (Math.random() * new Date().getTime()).toString(36).replace( /\./g , '');
+    }
+}
+
+var mediaStream = null;
+// reusable getUserMedia
+function captureUserMedia(success_callback) {
+    var session = {
+        audio: true,
+        video: {
+            // mediaSource: "screen", // whole screen sharing
+                mediaSource: "window", // choose a window to share
+            // mediaSource: "application", // choose a window to share
+            width: {max: '1920'},
+            height: {max: '1080'},
+            frameRate: {max: '10'}
+            }
+    }; 
+    navigator.getUserMedia(session, success_callback, function(error) {
+        console.error(error);
+    });
+}
+
+// UI events handling
+ function startRecord(stream) {  
+        recorder = RecordRTC(stream, {
+            type: 'video'
+        });     
+        recorder.startRecording();
+};
+
+
+function endRecord() {
+    recorder.stopRecording(postFiles);
+};
--- server_example/server.js
+++ server_example/server.js
@@ -1,10 +1,17 @@
-var http    = require("http");         
+var http    = require("http");       
+url = require('url'),
+path = require('path'),
+fs = require('fs');
+var uploadDir = __dirname;  
 var express = require("express");          
 var serveStatic = require('serve-static');  
 var socketIo = require("socket.io");      
 var easyrtc = require("../");
 var nodeMaria = require('node-mariadb');
-var mysql = require('mysql');           
+var mysql = require('mysql');  
+var mime = require('mime');
+var formidable = require('formidable');
+var util = require('util');         
 //var mongoose = require("mongoose")
 //var conString = "mongodb://localhost/mylearning"
 var bodyParser = require("body-parser");
@@ -12,9 +19,11 @@ process.title = "node-easyrtc";
 var app = express();
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/script'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 function BD() {
 var connection = mysql.createConnection({
     host: "learnweb.l3s.uni-hannover.de",
@@ -22,29 +31,26 @@ var connection = mysql.createConnection({
     user: "pairsearch",
     password: "Ywe9EOH9lxk6dIWk",
     database :"pairsearch"
-  });
-     return connection;
-};
-app.post("/room/:roomId", async (req, res) => {
-    var objBD = BD();
-    console.log(req.body.name);
-    objBD.query("INSERT INTO  `chat_logs` (  `chatlog_id` ,  `name` ,  `text` ,  `room_id` ,  `socket_id` ,  `timestamp` ) VALUES (NULL ,'"+req.body.name+"', '"+req.body.chat+"', '"+req.body.roomId+"',  '"+req.body.rtcId+"', CURRENT_TIMESTAMP)", function(error) {
-        if (error) {
-            console.log(error.message);
-        } else {
-            console.log('success');    
-        }
     });
-});
-
+    return connection;
+};
+// app.post("/room/:roomId", async (req, res) => {
+//     var objBD = BD();
+//     console.log(req.body.name);
+//     objBD.query("INSERT INTO  `chat_logs` (  `chatlog_id` ,  `name` ,  `text` ,  `room_id` ,  `socket_id` ,  `timestamp` ) VALUES (NULL ,'"+req.body.name+"', '"+req.body.chat+"', '"+req.body.roomId+"',  '"+req.body.rtcId+"', CURRENT_TIMESTAMP)", function(error) {
+//         if (error) {
+//             console.log(error.message);
+//         } else {
+//             console.log('success');    
+//         }
+//     });
+// });
 app.get('/', function (req, res) {   
-     res.sendFile(__dirname + "/view/start.html");
+     res.sendFile(__dirname + "/view/start.html"); 
 });
-
 app.get('/room/:roomId', function (req, res) {
     res.sendFile(__dirname + "/view/room.html");
 });
-
 // mongoose.Promise = Promise
 // var Chats = mongoose.model("Chats", {
 //     rtcId: String,
@@ -67,7 +73,6 @@ app.get('/room/:roomId', function (req, res) {
 //         console.error(error)
 //     }
 // })
-
 var webServer = http.createServer(app);
 // Start Socket.io so it attaches itself to Express server
 var socketServer = socketIo.listen(webServer, {"log level":1});
@@ -88,78 +93,53 @@ easyrtc.events.on("easyrtcAuth", function(socket,  easyrtcid, msg, socketCallbac
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
-    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
-});
-var myIceServers = [
-    {"url":"stun:stun01.sipphone.com"},
-    {"url":"stun:stun.ekiga.net"},
-    {"url":"stun:stun.fwdnet.net"},
-    {"url":"stun:stun.ideasip.com"},
-    {"url":"stun:stun.iptel.org"},
-    {"url":"stun:stun.rixtelecom.se"},
-    {"url":"stun:stun.schlund.de"},
-    {"url":"stun:stun.l.google.com:19302"},
-    {"url":"stun:stun1.l.google.com:19302"},
-    {"url":"stun:stun2.l.google.com:19302"},
-    {
-        "url":"turn:numb.viagenie.ca",
-        "username":"webrtc@live.com",
-        "credential":"muazkh"
-    },
-    {
-        "url":"turn:192.158.29.39:3478?transport=udp",
-        "username":"28224511:1379330808",
-        "credential":"JZEOEt2V3Qb0y27GRntt2u2PAYA="
-      },
-    {
-      "url":"turn:192.158.29.39:3478?transport=tcp",
-      "username":"28224511:1379330808",
-      "credential":"JZEOEt2V3Qb0y27GRntt2u2PAYA="
-    }
-  ];
-//   var myIceServers = [
-//     {"url":"stun:stun.l.google.com:19302"},
-//     {
-//         "url":"turn:192.158.29.39:3478?transport=udp",
-//         "username":"28224511:1379330808",
-//         "credential":"JZEOEt2V3Qb0y27GRntt2u2PAYA="
-//       },
-//     {
-//       "url":"turn:192.158.29.39:3478?transport=tcp",
-//       "username":"28224511:1379330808",
-//       "credential":"JZEOEt2V3Qb0y27GRntt2u2PAYA="
-//     }
-//   ];
-
-
-
-//easyrtc.setOption("appIceServers", myIceServers);
+// easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
+//     console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+//     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
+// });
 // Start EasyRTC server
 var rtc = easyrtc.listen(app, socketServer, function(err, rtcRef) {
     console.log("Initiated");
     easyrtc.setOption("sessionEnable", true);
     easyrtc.setOption("sessionCookieEnable", true);
-    // easyrtc.on("getIceConfig", function(connectionObj, callback){
-    //     var myIceServers=[
-    //       {"url":"stun:stun.easyrtc.com:3478"},
-    //       {
-    //         "url":        "turn:turn.easyrtc.com:3478",
-    //         "username":   "qwertre123",
-    //         "credential": "345yRTC!"
-    //       }
-    //     ];
-      
-    //     callback(null, myIceServers);
-    //   });
     rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
         console.log("roomCreate fired! Trying to create: " + roomName);
         appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
-    });
-
-    
+    });    
 });
 webServer.listen(8000, function () {
     console.log('listening on http://localhost:8000');
+});
+
+app.post("/room/:roomId/action1", function (request, response) {
+    
+});
+
+app.post("/room/:roomId/action2", function (request, response) {
+    
+});
+
+app.post("/room/:roomId", function (request, response)  {
+    var form = new formidable.IncomingForm();
+    var dir = !!process.platform.match(/^win/) ? '\\uploads\\' : '/uploads/';
+    form.uploadDir = __dirname + '/uploads';
+    form.keepExtensions = true;
+    form.maxFieldsSize = 10 * 1024 * 1024;
+    form.maxFields = 1000;
+    form.multiples = false;
+
+    form.parse(request, function(err, fields, files) {
+        var file = util.inspect(files);
+
+       // response.writeHead(200, getHeaders('Content-Type', 'application/json'));
+
+        var fileName = file.split('path:')[1].split('\',')[0].split(dir)[1].toString().replace(/\\/g, '').replace(/\//g, '');
+        var fileURL = 'http://' + 'localhost' + ':' + 8000 + '/uploads/' + fileName;
+
+       console.log('fileURL: ', fileURL);
+        response.write(JSON.stringify({
+            fileURL: fileURL
+        }));
+        response.end();
+    });
 });
